555854dff6376868747e634fdb8798cbbebe9d4c
alan-junqueira <contato.alanjunqueira@gmail.com>
2023-11-08 17:50:29 -0300

    feat: add button from primeng at list.

--- modulo-06-angular/aula2/src/app/pages/produtos/components/lista-item/lista-item.module.ts
+++ modulo-06-angular/aula2/src/app/pages/produtos/components/lista-item/lista-item.module.ts
@@ -3,15 +3,12 @@ import { CommonModule } from '@angular/common';
 import { ListaItemComponent } from './lista-item.component';
 import { ItemCardComponent } from './item-card/item-card.component';
 import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [ListaItemComponent, ItemCardComponent],
   providers: [],
   exports: [ListaItemComponent],
-  imports: [
-    CommonModule,
-    CardModule
-  ]
+  imports: [CommonModule, CardModule, ButtonModule],
 })
-
-export class ListaItemModule { }
+export class ListaItemModule {}
