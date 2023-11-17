69e8f842cf5c53b91f6a5c8eb93fcb8d228c4ac8
alan-junqueira <contato.alanjunqueira@gmail.com>
2023-11-17 18:40:59 -0300

    feat: add icons and create footer

--- modulo-06-angular/exercicio/project/src/app/components/components.module.ts
+++ modulo-06-angular/exercicio/project/src/app/components/components.module.ts
@@ -2,10 +2,31 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { NgIconsModule } from '@ng-icons/core';
+import {
+  radixInstagramLogo,
+  radixLinkedinLogo,
+  radixGithubLogo,
+} from '@ng-icons/radix-icons';
+import {
+  tablerBrandWhatsapp,
+  tablerBrandTwitterFilled,
+  tablerBrandFacebookFilled,
+} from '@ng-icons/tabler-icons';
 
 @NgModule({
   declarations: [FooterComponent, HeaderComponent],
   exports: [FooterComponent, HeaderComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    NgIconsModule.withIcons({
+      radixInstagramLogo,
+      radixLinkedinLogo,
+      radixGithubLogo,
+      tablerBrandWhatsapp,
+      tablerBrandTwitterFilled,
+      tablerBrandFacebookFilled,
+    }),
+  ],
 })
 export class ComponentsModule {}
--- modulo-06-angular/exercicio/project/src/app/components/footer/footer.component.ts
+++ modulo-06-angular/exercicio/project/src/app/components/footer/footer.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent {
-
+  logos = {
+    linkedin: 'radixLinkedinLogo',
+    instagram: 'radixInstagramLogo',
+    github: 'radixGithubLogo',
+    whatsApp: 'tablerBrandWhatsapp',
+    twitter: 'tablerBrandTwitterFilled',
+    facebook: 'tablerBrandFacebookFilled',
+  };
 }
